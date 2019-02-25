0575311bd22aeaa41a0cf73c0a9e6c05e7bc7ae7
olegphorostyna <olegphorostyna@gmail.com>
2019-02-25 19:19:56 +0200

    Basic server added

---
+++ ChatServerV3/chatserver.h
@@ -0,0 +1,26 @@
+#ifndef CHATSERVER_H
+#define CHATSERVER_H
+#include <QTcpSocket>
+#include <QTcpServer>
+
+#include <QObject>
+
+class ChatServer : public QObject
+{
+    Q_OBJECT
+public:
+    static const short int PORT = 8888;
+    explicit ChatServer(QObject *parent = nullptr);
+private:
+    QTcpServer *server;
+
+
+signals:
+
+public slots:
+    void newConnection();
+    //void readyRead();
+
+};
+
+#endif // CHATSERVER_H
