712d036706f7489628c78ce46fce745eb044304c
Yulian Fedirko <fedsrko08@gmail.com>
2019-03-04 13:49:46 +0200

    Overload functions in server DAL

--- DAL/Server/serverdb.h
+++ DAL/Server/serverdb.h
@@ -21,17 +21,25 @@ public:
     virtual~ServerDB();
 
     void                 AddNewUser(const QString& new_user_login, const QString& new_user_password);
+
     void                 UpdateIPPort(const QString& user_login,const QString& new_user_ip, const int& new_user_port);//by login 
 	void                 UpdateIPPort(const unsigned int& user_id, const QString& new_user_ip, const int& new_user_port);//by id
+
     QPair<QString, int>  GetIPPort(const QString& user_login, const QString& second_user_login);
     bool                 CheckUser(const QString& user_login, const QString& user_password);
+
     void                 AddFriend(const QString& user_login, const QString& second_user_login);
+	void                 AddFriend(const unsigned int& user_id, const unsigned int& second_user_id);
+
     unsigned int         GetIDByLogin(const QString& user_login);//If login don't exist return id = 0	
 	QString				 GetLoginByID(const unsigned int& user_id);// String will be empty if user id dont exist
 
 private:
     int           FindMaxID();//Find maximal id in the table, if something go wrong return -1
+
     bool          IsFriend(const QString& first_user_login, const QString& second_user_login);
+	bool          IsFriend(const unsigned int& first_user_id, const unsigned int& second_user_id);
+
     void          ErrorInfo();
 	bool	      IsLoginExist(const QString& user_login);// Check that user exist
 
