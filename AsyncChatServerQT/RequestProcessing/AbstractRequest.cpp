db2fbbf4247d2d1382f2cb3530b49b0c51ba5428
CODER <mazorchyk@gmail.com>
2019-03-04 23:40:02 +0200

    actually added

--- AsyncChatServerQT/RequestProcessing/AbstractRequest.cpp
+++ AsyncChatServerQT/RequestProcessing/AbstractRequest.cpp
@@ -5,7 +5,8 @@
 AbstractRequest::AbstractRequest(QByteArray& r, DAL &d) : database(d)
 {
 }
-
+AbstractRequest::AbstractRequest(QByteArray& r)
+{}
 
 
 void AbstractRequest::connected() {
--- AsyncChatServerQT/RequestProcessing/AbstractRequest.h
+++ AsyncChatServerQT/RequestProcessing/AbstractRequest.h
@@ -6,7 +6,9 @@
 #include "../DAL/dal.h"
 #include <QTcpSocket>
 #include <QByteArray>
-
+#include "../Parser&Structs/parser.h"
+#include "../Parser&Structs/request_types.h"
+#include"../DAL/Server/serverdb.h"
 
 //using namespace std;
 
@@ -14,7 +16,7 @@ class AbstractRequest : public QObject {
 	Q_OBJECT
 public:
 	AbstractRequest(QByteArray&, DAL &);
-
+	AbstractRequest(QByteArray&);
 	//Receives socket initalized by client or a socket initialized by server.
 	//Depending from the type of request one of that sockets may be nullptr.
 	//That sockets will be used to send correct responses.
@@ -22,8 +24,10 @@ public:
 protected:
 	//every derived class will override this function in order to return
 	//appropriate response according to incoming request. That response will be stored in 
-	//string fields and will be processed but sendResponde()
-	virtual void prepareResponse() = 0;
+	
+        //string fields and will be processed but sendResponde()
+	ServerDB db;
+        virtual void prepareResponse() = 0;
 	
 	//handel to acccess databse
 	DAL& database;
