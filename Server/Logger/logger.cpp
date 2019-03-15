fddc5aeeb77dfa070f2f2b1139c103d0529fc35e
CODER <mazorchyk@gmail.com>
2019-03-15 13:36:09 +0200

    updated logger fixed type bug

--- Server/Logger/logger.cpp
+++ Server/Logger/logger.cpp
@@ -21,35 +21,36 @@ void Logger::log(QByteArray raw_data) {
     
     QString outingString;  // date (d/m/y) time (h/m/s)
 	QString txt = QDateTime::currentDateTime().toString("dd:MM:yyyy hh:mm:ss ");
-    quint8 type = Parser::getRequestType(raw_data);
+	quint8 type = Parser::getRequestType(raw_data);
+	QString for_empty = ConvertQuint8ToString(type) +")" + txt + "+ empty{}";
+	QString for_struct;
     switch (type) {
-      case (quint8)ServerRequests::LOGIN_SUCCEED:
-		outingString = "Server output: LOGIN_SUCCEED ( " + ConvertQuint8ToString(type) +
-		  ")"+txt+"+ empty{}";
-        qDebug() << outingString << "\n";
-        WriteLogToFile(QString("LOG.txt"), outingString);
-        break;
-      case (quint8)ServerRequests::LOGIN_FAILED:
-        outingString = "Server output: LOGIN_FAILED( " + ConvertQuint8ToString(type) +
-                       ")" + txt + "+ empty{}";
-        qDebug() << outingString << "\n";
+      case (quint8)ServerRequests::LOGIN_SUCCEED: {
+		outingString = "Server output: LOGIN_SUCCEED ( " + for_empty;
+		qDebug() << outingString << "\n";
 		WriteLogToFile(QString("LOG.txt"), outingString);
-        break;
-      case (quint8)ServerRequests::REGISTER_FAILED:
-        outingString = "Server output: REGISTER_FAILED( " + ConvertQuint8ToString(type) +
-                       ")" + txt + "+ empty{}";
-        qDebug() << outingString << "\n";
-        WriteLogToFile(QString("LOG.txt"), outingString);
-        break;
-      case (quint8)ServerRequests::FRIEND_REQUEST_FAILED:
-        outingString = "Server output: FRIEND_REQUEST_FAILED( " +
-		  ConvertQuint8ToString(type) + ") " + txt + "+ empty{}";
-        qDebug() << outingString << '\n';
-        WriteLogToFile(QString("LOG.txt"), outingString);
-        break;
+		break;
+	  }
+	  case (quint8)ServerRequests::LOGIN_FAILED: {
+		outingString = "Server output: LOGIN_FAILED( " + for_empty;
+		qDebug() << outingString << "\n";
+		WriteLogToFile(QString("LOG.txt"), outingString);
+		break;
+	  }
+      case (quint8)ServerRequests::REGISTER_FAILED:{
+		outingString = "Server output: REGISTER_FAILED( " + for_empty;
+		qDebug() << outingString << "\n";
+		WriteLogToFile(QString("LOG.txt"), outingString);
+		break;
+	  }
+	  case (quint8)ServerRequests::FRIEND_REQUEST_FAILED: {
+		outingString = "Server output: FRIEND_REQUEST_FAILED( " + for_empty;
+		qDebug() << outingString << '\n';
+		WriteLogToFile(QString("LOG.txt"), outingString);
+		break;
+	  }
       case (quint8)ServerRequests::FRIEND_REQUEST_SUCCEED: {
-        outingString = "Server output: FRIEND_REQUEST_SUCCEED( " +
-		  ConvertQuint8ToString(type) + ") " + txt + "+empty{}";
+		outingString = "Server output: FRIEND_REQUEST_SUCCEED( " + for_empty;
         qDebug() << outingString << "\n";
         WriteLogToFile(QString("LOG.txt"), outingString);
         break;
@@ -57,8 +58,8 @@ void Logger::log(QByteArray raw_data) {
       case (quint8)ServerRequests::REGISTER_SUCCEED: {
         RegisterSuccessInfo out;
         out = Parser::ParseAsRegisterSuccessInfo(raw_data);
-        outingString = "Server  output: REGISTER_SUCCEED(" + ConvertQuint8ToString(type) +
-                       ")+RegisterSuccessInfo{ " + QString(out.id) + " }" + txt+ '\n';
+		outingString = "Server  output: REGISTER_SUCCEED(" + ConvertQuint8ToString(type) +
+		  ")" + Log_REGISTER_SUCCEED(out);
 
         qDebug() << outingString << "\n";
         WriteLogToFile(QString("LOG.txt"), outingString);
@@ -67,10 +68,8 @@ void Logger::log(QByteArray raw_data) {
       case (quint8)ServerRequests::ADD_FRIEND_REQUEST: {
         AddFriendInfo out;
         out = Parser::ParseAsAddFriendInfo(raw_data);
-        outingString = "Server  output: ADD_FRIEND_REQUEST(" + ConvertQuint8ToString(type) +
-                       +")+AddFriendInfo{ " + out.requester_ip.toString() +
-                       ", " + out.requester_login + ", " +
-                       QString(out.requester_port) + " }" + txt + '\n';
+		outingString = "Server  output: ADD_FRIEND_REQUEST(" + ConvertQuint8ToString(type) +
+		  +")" + Log_ADD_FRIEND_REQUEST(out);
         qDebug() << outingString << "\n";
         WriteLogToFile(QString("LOG.txt"), outingString);
         break;
@@ -79,12 +78,31 @@ void Logger::log(QByteArray raw_data) {
         FriendUpdateInfo out;
         out = Parser::ParseAsFriendUpdateInfo(raw_data);
         outingString = "Server  output: FRIEND_UPDATE_INFO(" + ConvertQuint8ToString(type) +
-                       ") + FriendUpdateInfo{ " + QString(out.id) + ", " +
-                       out.ip.toString() + ", " + QString(out.port) + " }" + txt + '\n';
+                       ") + FriendUpdateInfo{ " +"\nID:" +QString(out.id) + ",\nIP: " +
+                       out.ip.toString() + ",\n PORT: " + QString(out.port) + " }" + txt + '\n';
         qDebug() << outingString << "\n";
         WriteLogToFile(QString("LOG.txt"), outingString);
         break;
       }
     }
   }
+}
+
+QString Logger::Log_REGISTER_SUCCEED(RegisterSuccessInfo & out)
+{
+  QString txt = QDateTime::currentDateTime().toString("dd:MM:yyyy hh:mm:ss ");
+  return "RegisterSuccessInfo{ \nID:" + QString(out.id) + " \n}" + txt + '\n';
+}
+
+QString Logger::Log_ADD_FRIEND_REQUEST(AddFriendInfo & out)
+{
+  QString txt = QDateTime::currentDateTime().toString("dd:MM:yyyy hh:mm:ss ");
+  return "AddFriendInfo{ \nRequester_ip: " + out.requester_ip.toString() + ", \nPort: " +
+					   QString(out.requester_port) + ", \nRequester Login: " + out.requester_login + " }" + txt + '\n';
+}
+
+QString Logger::Log_FRIEND_UPDATE_INFO(FriendUpdateInfo & out)
+{
+  QString txt = QDateTime::currentDateTime().toString("dd:MM:yyyy hh:mm:ss ");
+  return txt;
 }
--- Server/Logger/logger.h
+++ Server/Logger/logger.h
@@ -12,5 +12,8 @@ class Logger {
    static void WriteLogToFile(QString&,QString&);
    static QString ConvertQuint8ToString(quint8& num);
    static void log(QByteArray);   
+   static QString Log_REGISTER_SUCCEED(RegisterSuccessInfo &out);
+   static QString Log_ADD_FRIEND_REQUEST(AddFriendInfo &out);
+   static QString Log_FRIEND_UPDATE_INFO(FriendUpdateInfo &out);
    
 };
