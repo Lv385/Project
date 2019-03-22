eff1584e395e9f869a48c3199d616cded47d0df5
CODER <mazorchyk@gmail.com>
2019-03-22 22:21:11 +0200

    very small commit

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,11 +1,17 @@
 #include <QtCore/QCoreApplication>
 #include "AsyncServer.h"
 #include <iostream>
-
+#include"sqldal.h"
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+  /*UnitOfWork db{};
+  auto user = db.GetEntity<User>();
+  user->id = 4;
+  user->GetUser();
+  std::cout << user->login.toStdString();*/
 
 	AsyncServer async_server; // run server
 	return a.exec();
