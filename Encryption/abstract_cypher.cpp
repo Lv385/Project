7a4f5cf0baee7cb06c62d1d1ba8ec16b97669e12
CODER <mazorchyk@gmail.com>
2019-03-27 11:14:27 +0200

    finally fixed string

--- Encryption/abstract_cypher.cpp
+++ Encryption/abstract_cypher.cpp
@@ -5,6 +5,6 @@ QString AbstractCypher::HashString(QString s)
 {
   QByteArray ba = s.toUtf8();
   QByteArray hashed_arr =QCryptographicHash::hash(ba, QCryptographicHash::Md5).toHex();
-  QString DataAsString = QTextCodec::codecForMib(1015)->toUnicode(hashed_arr);
-  return DataAsString;
+  QString str= QString::fromUtf8(hashed_arr);
+  return str ;
 }
