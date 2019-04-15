5157868ba536ca98206f31685458fafad714e4ab
Markiyan Kalika <varxdar.mark@gmail.com>
2019-04-15 14:50:41 +0300

    added save/get friend requests

--- Peer/dataaccessor.h
+++ Peer/dataaccessor.h
@@ -9,28 +9,29 @@ class DataAccessor {
   DataAccessor();
   ~DataAccessor();
 
-  QString get_login_by_id(const unsigned user_id);
-  QPair<QString, int> get_ip_port(const unsigned user_id);
+  QString get_login_by_id(const unsigned& user_id);
+  QPair<QString, int> get_ip_port(const unsigned& user_id);
   QVector<Friend> get_friends();
-  QVector<Message> get_messages(const QString user_login);
-  QVector<Message> get_messages(unsigned user_id);
+  QVector<Message> get_messages(const QString& user_login);
+  QVector<Message> get_messages(const unsigned& user_id);
 
-  Friend get_friend(const unsigned user_id);
-  unsigned get_id_by_login(const QString user_login);
-  bool get_friends_status(const unsigned user_id);
+  Friend get_friend(const unsigned& user_id);
+  unsigned get_id_by_login(const QString& user_login);
+  bool get_friends_status(const unsigned& user_id);
 
-  void set_friend_status(const unsigned user_id, const bool status);
+  void set_friend_status(const unsigned& user_id, const bool& status);
 
   void AddFriend(const Friend& friend_info);
   void AddFriend(const unsigned& user_id, const QString& login,
                  const QString& ip = 0, const unsigned& port = 0,
                  const QString& name = 0, const QString& surname = 0);
   void AddMessageToDB(const QString& msg, const unsigned& user_id, const unsigned& owner_id);
-  void AddMessageToDB(const Message& message) {
-    message_->AddNewMessage(message);
- }
+  void AddMessageToDB(const Message& message);
+   
+  void SaveRequest(const FriendRequest& friend_request);
+  QVector<FriendRequest> GetRequests();
 
-  void UpdateIPPort(const unsigned user_id, const QString new_ip, const unsigned new_port);
+  void UpdateIPPort(const unsigned& user_id, const QString& new_ip, const unsigned& new_port);
 
  private:
   dal::UnitOfWork db_;
