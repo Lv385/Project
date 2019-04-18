c113170a1ae53f51cb5d1c820a177cd26ca30616
CODER <mazorchyk@gmail.com>
2019-04-18 15:40:58 +0300

    fixed inv mix columns not working xor round key

--- Server/main.cpp
+++ Server/main.cpp
@@ -13,12 +13,12 @@ int main(int argc, char *argv[]) {
 	AsyncServer async_server; // run server
   //HWND hWnd = GetConsoleWindow();
   //ShowWindow(hWnd, SW_HIDE);
-/*
+
   QString str = "hello_oleksa_libomyr_marko";
   AbstractCypher *cpp;
   cpp = new AESCypher();
   QByteArray arr= cpp->ConvertStringToByteArr(str);
   cpp->Encrypt(arr,str);
-  cpp->Decrypt(arr, str);*/
+  cpp->Decrypt(arr, str);
  return a.exec();
 }
